03f2c679ebd146a028a34404dbe42a31a2f9d69c
Naufal <sadamdani28@gmail.com>
2025-09-02 19:18:54 +0700

    Soal 1

---
+++ Week 2/src/soal1.dart
@@ -0,0 +1,12 @@
+void hello(){
+  for (int i = 0; i < 10; i++) {
+    print('Hello, ${i + 2}');
+  }
+}
+
+void main(){
+  hello();
+  for (int i = 18; i >8; i--) {
+    print('Nama Saya adalah Naufal Sekarang Berumur ${i}');
+  }
+}
