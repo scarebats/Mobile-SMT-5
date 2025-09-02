5ce20233e9f11b97f504eca982ef0139f6b3d112
Naufal <sadamdani28@gmail.com>
2025-09-02 20:17:07 +0700

    Tugas Praktikum - #02 | Pengantar Bahasa Pemrograman Dart - Bagian 1

---
+++ Week 2/src/late.dart
@@ -0,0 +1,10 @@
+void main() {
+  late String message;
+
+  // message belum diisi, jadi kalau dipanggil sekarang akan error
+  // print(message); // Error
+
+  // Inisialisasi di kemudian waktu
+  message = "Hello Naufal!";
+  print(message); // Hello Naufal!
+}
---
+++ Week 2/src/null.dart
@@ -0,0 +1,8 @@
+void main() {
+  String? nama;                 // deklarasi nullable (bisa null / string)
+  nama = "Ahmad Naufal ";       // sekarang diisi string
+  print("Nama saya: $nama");
+
+  nama = null;                  // juga boleh null
+  print("Nama sekarang: $nama");
+}
